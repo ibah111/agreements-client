b197872151efacda4b1e18462b5fb610bb721669
Ibah <starlord77@mail.ru>
2023-03-16 11:46:13 +0300

    Добавил локализацию

--- src/lib/createTheme.ts
+++ src/lib/createTheme.ts
@@ -1,14 +1,20 @@
 import { createTheme as createThemeOrigin, PaletteMode } from "@mui/material";
+import { ruRU } from "@mui/material/locale";
+import { ruRU as ruRUGrid } from "@mui/x-data-grid-premium";
 export default function createTheme(mode?: PaletteMode) {
-  return createThemeOrigin({
-    palette: {
-      mode,
-    },
-    components: {
-      MuiButton: { defaultProps: { color: "primary" } },
-      MuiButtonGroup: {
-        defaultProps: { color: "primary" },
+  return createThemeOrigin(
+    {
+      palette: {
+        mode,
+      },
+      components: {
+        MuiButton: { defaultProps: { color: "primary" } },
+        MuiButtonGroup: {
+          defaultProps: { color: "primary" },
+        },
       },
     },
-  });
+    ruRU,
+    ruRUGrid
+  );
 }
