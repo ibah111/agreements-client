5a8653be02a266e332351ac7f321e9f7a59f0b3e
Ibah <starlord77@mail.ru>
2023-03-16 11:49:05 +0300

    Поправил диалоговое окно

--- src/pages/Add_agreements/AddAgreement.tsx
+++ src/pages/Add_agreements/AddAgreement.tsx
@@ -11,7 +11,7 @@ import {
   Typography,
 } from "@mui/material";
 
-import React, { useState } from "react";
+import React from "react";
 interface GridContainerProps {
   children: React.ReactNode[];
 }
@@ -22,14 +22,12 @@ const GridContainer = ({ children }: GridContainerProps) => (
     ))}
   </Grid>
 );
-export default function AddAgreementDialog() {
-  const [open, setOpen] = React.useState(true);
-  const hadleClickOpen = () => {
-    setOpen(true);
-  };
-  const handleClickClose = () => {
-    setOpen(false);
-  };
+
+interface AddAgreementDialogProps {
+  open: boolean;
+  onClose: () => void;
+}
+export default function AddAgreementDialog(props: AddAgreementDialogProps) {
   const purpose = [
     {
       label: "Задолженность взыскана банком",
@@ -44,84 +42,88 @@ export default function AddAgreementDialog() {
       label: "Индексация",
     },
   ];
-
   return (
-    <Dialog open={open} maxWidth="lg" fullWidth>
-      <DialogTitle alignSelf={"center"}>Data title</DialogTitle>
-      <Divider />
-      <DialogContent sx={{ flexGrow: 1 }}>
-        <GridContainer>
-          <Grid>
-            <TextField label="Номер в БД" type={"number"} value={"number"} />
-          </Grid>
-          <Grid>
-            <Typography>Дата последней проверки</Typography>
-          </Grid>
-          <Grid>
+    <>
+      <Dialog open={props.open} onClose={props.onClose} maxWidth="lg" fullWidth>
+        <DialogTitle alignSelf={"center"}>Data title</DialogTitle>
+        <Divider />
+        <DialogContent sx={{ flexGrow: 1 }}>
+          <GridContainer>
+            <Grid>
+              <TextField label="Номер в БД" type={"number"} value={"number"} />
+            </Grid>
+            <Grid>
+              <Typography>Дата последней проверки</Typography>
+            </Grid>
+            <Grid>
+              <TextField type={"date"} />
+            </Grid>
+            <Grid>
+              <Typography>Дата заключения</Typography>
+            </Grid>
             <TextField type={"date"} />
-          </Grid>
-          <Grid>
-            <Typography>Дата заключения</Typography>
-          </Grid>
-          <TextField type={"date"} />
-          <Autocomplete
-            options={purpose}
-            sx={{ width: 300 }}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label="Назначение"
-                sx={{
-                  alignSelf: "center",
-                }}
-              />
-            )}
-          />
-          <Grid>
-            <TextField label="Судебная сумма" />
-          </Grid>
-          <Grid>
-            <TextField label="Сумма долга" />
-          </Grid>
-          <Grid>
-            <TextField label="Сумма пересчета" />
-          </Grid>
-          <Grid>
-            <TextField label="Дисконт" />
-          </Grid>
-          <Grid>
-            <TextField label="Число платежа" />
-          </Grid>
-          <Grid>
-            <TextField label="Регистрационный документ" />
-          </Grid>
-          <Grid>
-            <TextField label="Конечный документ" />
-          </Grid>
-          <Grid>
-            <TextField label="Действия для получения листа" />
-          </Grid>
-          <Grid>
-            <TextField label="Комментарий" />
-          </Grid>
-          <Grid>
-            <TextField label="Ссылка на задачу" />
-          </Grid>
-        </GridContainer>
-      </DialogContent>
-      <Divider />
-      <DialogActions>
-        <Button variant="contained" sx={{ width: "100", alignSelf: "center" }}>
-          Send
-        </Button>
-        <Button
-          variant="contained"
-          sx={{ width: "auto", alignSelf: "center" }}
-          onClick={handleClickClose}
-        >
-          Закрыть окно
-        </Button>
-      </DialogActions>
-    </Dialog>
+            <Autocomplete
+              options={purpose}
+              sx={{ width: 300 }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Назначение"
+                  sx={{
+                    alignSelf: "center",
+                  }}
+                />
+              )}
+            />
+            <Grid>
+              <TextField label="Судебная сумма" />
+            </Grid>
+            <Grid>
+              <TextField label="Сумма долга" />
+            </Grid>
+            <Grid>
+              <TextField label="Сумма пересчета" />
+            </Grid>
+            <Grid>
+              <TextField label="Дисконт" />
+            </Grid>
+            <Grid>
+              <TextField label="Число платежа" />
+            </Grid>
+            <Grid>
+              <TextField label="Регистрационный документ" />
+            </Grid>
+            <Grid>
+              <TextField label="Конечный документ" />
+            </Grid>
+            <Grid>
+              <TextField label="Действия для получения листа" />
+            </Grid>
+            <Grid>
+              <TextField label="Комментарий" />
+            </Grid>
+            <Grid>
+              <TextField label="Ссылка на задачу" />
+            </Grid>
+          </GridContainer>
+        </DialogContent>
+        <Divider />
+        <DialogActions>
+          <Button
+            variant="contained"
+            sx={{ width: "100", alignSelf: "center" }}
+          >
+            Send
+          </Button>
+          <Button
+            variant="contained"
+            sx={{ width: "auto", alignSelf: "center" }}
+            onClick={props.onClose}
+          >
+            Закрыть окно
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
