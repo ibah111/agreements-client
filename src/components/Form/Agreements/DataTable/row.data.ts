bcbe5f90a3643eb468b314afa06a59e9f673910e
Ibah <starlord77@mail.ru>
2023-03-17 12:22:53 +0300

    Убрал дубль

--- src/components/Form/Agreements/DataTable/row.data.ts
+++ src/components/Form/Agreements/DataTable/row.data.ts
@@ -1,25 +1,8 @@
 import axios from "axios";
 import { plainToInstance } from "class-transformer";
 import { baseRequest } from "../../../../utils/baseRequest";
-import { LawAct } from "@contact/models";
+import { Agreement } from "../../../../api/getAgreement";
 
-export class Agreement {
-  r_law_act_id: number;
-  LawAct: LawAct;
-  last_check_date: Date;
-  conclusion_date: Date;
-  purpose: number;
-  court_sum: number;
-  debt_sum: number;
-  recalculation_sum: number | null;
-  discount_sum: number | null;
-  month_pay_day: number;
-  reg_doc: boolean;
-  finish_doc: boolean;
-  actions_for_get: string | null;
-  comment: string;
-  task_link: string;
-}
 export default async function Rows(data: Agreement) {
   try {
     const res = await baseRequest.post<Agreement>("/Agreements/getAll", {
