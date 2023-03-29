582e4aac08f43c110a160a2a1af2c26bfa8e829f
Ibah <starlord77@mail.ru>
2023-03-29 14:53:12 +0300

    Расширил ошибку

--- src/utils/processError.ts
+++ src/utils/processError.ts
@@ -1,5 +1,7 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+import { enqueueSnackbar } from "notistack";
 import callMessage from "./callMessage";
+import { Observable, retry, Subscriber } from "rxjs";
 
 export default function processError(e: unknown) {
   if (axios.isAxiosError(e)) {
@@ -7,6 +9,27 @@ export default function processError(e: unknown) {
       callMessage(e.response.data?.message || "Неизвестная ошибка", {
         variant: "error",
       });
+      return enqueueSnackbar(`${e.message} ${e.cause}, ${e.code}`, {
+        variant: "error",
+      });
     }
   }
+}
+export function createError<T>(subscriber: Subscriber<T>) {
+  return async (e: unknown) => subscriber.error(await processError(e));
+}
+export function createNextDefault<T>(subscriber: Subscriber<T>) {
+  return (res: AxiosResponse<T>) => {
+    subscriber.next(res.data);
+    subscriber.complete();
+  };
+}
+export function createRetry<T>() {
+  return retry<T>({
+    delay: (err) =>
+      new Observable((subscriber) => {
+        if (err === "retry") subscriber.next();
+        subscriber.complete();
+      }),
+  });
 }
