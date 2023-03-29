6db93d8500aaf841245b1318c72f5f18bf767804
Ibah <starlord77@mail.ru>
2023-03-29 14:56:31 +0300

    Поиск по контакт таблице

---
+++ src/pages/Main/AgreementDialog/Search/index.tsx
@@ -0,0 +1,39 @@
+import { Grid } from "@mui/material";
+import { DataGridPremium } from "@mui/x-data-grid-premium";
+import React from "react";
+import searchContact from "../../../../api/searchContact";
+import { useAppDispatch } from "../../../../Reducer";
+import { setLoadingResults, setResults } from "../../../../Reducer/Results";
+import Find from "./Find";
+
+export default function Search() {
+  const dispatch = useAppDispatch();
+
+  const Click = () => {
+    dispatch(setLoadingResults(true));
+    searchContact().subscribe({
+      next: (res) => {
+        dispatch(setResults(res));
+        dispatch(setLoadingResults(false));
+      },
+      error: () => {
+        setLoadingResults(false);
+      },
+    });
+  };
+  return (
+    <>
+      <Grid
+        item
+        container
+        spacing={1}
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Find onClick={Click} loading={false} />
+      </Grid>
+      <DataGridPremium columns={[]} rows={[]}></DataGridPremium>
+    </>
+  );
+}
