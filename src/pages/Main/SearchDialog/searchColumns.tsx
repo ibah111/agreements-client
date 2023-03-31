a5291f03d26d79d881325bae1b7b5a5a72a3c063
Ibah <starlord77@mail.ru>
2023-03-31 16:21:56 +0300

    Колонки таблицы

--- src/pages/Main/SearchDialog/searchColumns.tsx
+++ src/pages/Main/SearchDialog/searchColumns.tsx
@@ -1,9 +1,10 @@
-import { GridColDef } from "@mui/x-data-grid-premium";
+import { GridColDef, GridValueFormatterParams } from "@mui/x-data-grid-premium";
 import { Results } from "../../../Reducer/Results";
 
 export const searchColumns: GridColDef<Results>[] = [
   {
-    headerName: "Ссылка на person",
+    headerName: "Ссылка на челоевка",
+    description: "Ссылка на person",
     field: "r_person_id",
     width: 150,
     type: "number",
@@ -31,5 +32,46 @@ export const searchColumns: GridColDef<Results>[] = [
     field: "r_portfolio_id",
     width: 150,
     type: "date",
+    valueGetter: (params) => {
+      return "";
+    },
+  },
+  {
+    headerName: "Платеж",
+    field: "is_confirmed",
+    width: 50,
+    type: "string",
+    valueGetter: (params) => {
+      return params.row.DebtCalc.is_confirmed;
+    },
+    valueFormatter: (params: GridValueFormatterParams<number>) => {
+      if ((params.value = 1)) {
+        return "Принят";
+      } else {
+        return "Не принят";
+      }
+    },
+  },
+  {
+    headerName: "Ответ банка",
+    field: "is_cancel",
+    width: 50,
+    type: "string",
+    valueGetter: (params) => {
+      return params.row.DebtCalc.is_cancel;
+    },
+    valueFormatter: (params: GridValueFormatterParams<number>) => {
+      if ((params.value = 0)) {
+        return "Не отменен";
+      } else {
+        return "Отменен";
+      }
+    },
+  },
+  {
+    headerName: "Дата в банке",
+    field: "calc_date",
+    type: "date",
+    width: 100,
   },
 ];
