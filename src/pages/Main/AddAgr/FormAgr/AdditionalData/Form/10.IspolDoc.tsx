254b65e948e9bab03299a5797b86f2ab99a66361
Ibah <starlord77@mail.ru>
2023-04-07 14:31:35 +0300

    Закомментил

--- src/pages/Main/AddAgr/FormAgr/AdditionalData/Form/10.IspolDoc.tsx
+++ src/pages/Main/AddAgr/FormAgr/AdditionalData/Form/10.IspolDoc.tsx
@@ -1,11 +1,15 @@
-import { Grid, TextField } from "@mui/material";
+import { CheckBox } from "@mui/icons-material";
+import { FormControlLabel } from "@mui/material";
+// import { useDispatch } from "react-redux";
+// import { useAppSelector } from "../../../../../../Reducer";
+// import { setAgreementProperty } from "../../../../../../Reducer/Agreement";
 
 export default function IspolDoc() {
+  // const dispatch = useDispatch();
+  // const agreement = useAppSelector((state) => state.Agreement);
   return (
     <>
-      <Grid item spacing={3}>
-        <TextField label="ИД" />
-      </Grid>
+      <FormControlLabel label="ИД" control={<CheckBox />} />
     </>
   );
 }
