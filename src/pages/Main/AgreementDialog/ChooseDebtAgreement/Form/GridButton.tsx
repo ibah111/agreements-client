825dc6f4346b56ee7f8e663959d867a1bc2f95ff
Ibah <starlord77@mail.ru>
2023-04-14 10:07:58 +0300

    удалил кнопку

--- src/pages/Main/AgreementDialog/ChooseDebtAgreement/Form/GridButton.tsx
+++
@@ -1,9 +0,0 @@
-import { Button, Grid } from "@mui/material";
-
-export default function GridButton() {
-  return (
-    <Grid item xs={2}>
-      <Button variant="contained">Привязать долг</Button>;
-    </Grid>
-  );
-}
