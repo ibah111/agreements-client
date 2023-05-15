79f465939af305609d706076770a2942b545e4c4
Ibah <starlord77@mail.ru>
2023-05-15 13:56:23 +0300

    обработчик ошибки формы соглашения

--- src/utils/processError.ts
+++ src/utils/processError.ts
@@ -2,8 +2,48 @@ import axios, { AxiosResponse } from "axios";
 import { enqueueSnackbar } from "notistack";
 import callMessage from "./callMessage";
 import { Observable, retry, Subscriber } from "rxjs";
+import { ValidationError } from "class-validator";
+import { TranslateMessage } from "../Hooks/Validation/checker";
+import { store } from "../Reducer";
+import { addMessage } from "../Reducer/Message";
 
-export default function processError(e: unknown) {
+function objectKeys<T extends {}>(obj: T) {
+  return Object.keys(obj) as Array<keyof T>;
+}
+
+export function isValidationErrors(e: unknown): e is ValidationError[] {
+  if (e instanceof Array)
+    if (e.length > 0) if (e[0] instanceof ValidationError) return true;
+  return false;
+}
+
+export default function processError(e: unknown, name?: string) {
+  if (isValidationErrors(e)) {
+    for (const error of e) {
+      if (error.constraints) {
+        const keys = objectKeys(error.constraints);
+        let errorName = ``;
+        let options: Record<string, string> = {};
+        const errorMessage = error.constraints[keys[0]];
+        if (errorMessage.startsWith("{")) {
+          const JsonMessage = JSON.parse(errorMessage) as TranslateMessage;
+          errorName = JsonMessage.name;
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          options = JsonMessage.options;
+        } else {
+          errorName = errorMessage;
+        }
+        //доделать ошибку при невведенных данных
+        store.dispatch(
+          addMessage({
+            message: `Ошибка: ${errorName}`,
+            options: { variant: "error" },
+          })
+        );
+      }
+    }
+  }
+
   if (axios.isAxiosError(e)) {
     if (e.response) {
       callMessage(e.response.data?.message || "Неизвестная ошибка", {
