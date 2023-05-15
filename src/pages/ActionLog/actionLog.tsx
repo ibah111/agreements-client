3443ba25428631a2c7c05de4b650e23d642465aa
Ibah <starlord77@mail.ru>
2023-05-15 15:38:00 +0300

    Поправи компонент переходов

--- src/pages/ActionLog/actionLog.tsx
+++ src/pages/ActionLog/actionLog.tsx
@@ -1,11 +1,12 @@
 import { Box, Button, Grid } from "@mui/material";
 import { DataGridPremium } from "@mui/x-data-grid-premium";
+import { Link } from "react-router-dom";
 
 export default function ActionLog() {
   return (
     <Box>
       <Grid>
-        <Button LinkComponent={"a"} href="/" variant="contained">
+        <Button component={Link} to="/" variant="contained">
           Назад
         </Button>
       </Grid>
