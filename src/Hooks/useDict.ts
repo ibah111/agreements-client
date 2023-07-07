23e4ef15e658df9325fdd09b0ef1a63b47dab5c9
Ibah <starlord77@mail.ru>
2023-07-07 17:46:01 +0300

    perf(dict): useDict использует selector для обновления своего state

--- src/Hooks/useDict.ts
+++ src/Hooks/useDict.ts
@@ -1,19 +1,22 @@
-import { Dict } from "@contact/models";
 import React from "react";
 import getDict from "../api/getDict";
-import { useAppDispatch, useAppSelector } from "../Reducer";
+import { RootState, useAppDispatch, useAppSelector } from "../Reducer";
 import { setDict } from "../Reducer/Dict";
-
+import { createSelector } from "@reduxjs/toolkit";
+import { Dict } from "@contact/models";
+const selector = createSelector(
+  (state: RootState) => state.Dict,
+  (_: unknown, id: number) => id,
+  (state: Record<number, Dict[]>, id: number) => state[id] || []
+);
 export default function useDict(id: number) {
-  const dict = useAppSelector((state) => state.Dict[id]);
+  const dict = useAppSelector((state) => selector(state, id));
   const dispatch = useAppDispatch();
   React.useEffect(() => {
-    if (!dict) {
-      const sub = getDict(id).subscribe((res: Dict[]) =>
-        dispatch(setDict(res))
-      );
+    if (dict.length === 0) {
+      const sub = getDict(id).subscribe((res) => dispatch(setDict(res)));
       return sub.unsubscribe.bind(sub);
     }
   }, [dict, dispatch, id]);
-  return dict || [];
+  return dict;
 }
