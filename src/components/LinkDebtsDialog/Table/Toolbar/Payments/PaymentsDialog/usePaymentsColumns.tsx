099e67cb4555634a89fe4d23c761dd1d749dbf48
Ibah <starlord77@mail.ru>
2023-07-10 09:31:57 +0300

    perf(payments): фильтр назначения платежа

--- src/components/LinkDebtsDialog/Table/Toolbar/Payments/PaymentsDialog/usePaymentsColumns.tsx
+++ src/components/LinkDebtsDialog/Table/Toolbar/Payments/PaymentsDialog/usePaymentsColumns.tsx
@@ -1,46 +1,57 @@
 import { GridColDef } from "@mui/x-data-grid-premium";
 import { DebtCalcInstance } from "../../../../../../Models/DebtCalc";
-const columns: GridColDef<DebtCalcInstance>[] = [
-  {
-    align: "center",
-    headerAlign: "center",
-    headerName: "Сумма",
-    field: "sum",
-    width: 200,
-    type: "number",
-  },
-  {
-    align: "center",
-    headerAlign: "center",
-    headerName: "Дата",
-    type: "date",
-    valueGetter: (params) => (params.row.dt ? params.row.dt.toDate() : null),
-    field: "dt",
-    width: 200,
-  },
-  {
-    align: "center",
-    headerAlign: "center",
-    headerName: "Комментарий",
-    type: "string",
-    field: "dsc",
-    width: 400,
-    valueGetter: (params) => {
-      return params.row.dsc;
-    },
-  },
-  {
-    align: "center",
-    headerAlign: "center",
-    headerName: "Назначение платежа",
-    type: "string",
-    field: "PurposeDict",
-    width: 400,
-    valueGetter: (params) => {
-      if (params.row.PurposeDict?.name) return params.row.PurposeDict?.name;
-    },
-  },
-];
+import React from "react";
+import useDict from "../../../../../../Hooks/useDict";
+
 export default function usePaymentsColumns() {
+  const purposes = useDict(130);
+  const selectPurposes = React.useMemo(
+    () =>
+      purposes.map((item) => ({
+        value: item.code,
+        label: item.name,
+      })),
+    [purposes]
+  );
+  const columns = React.useMemo<GridColDef<DebtCalcInstance>[]>(
+    () => [
+      {
+        align: "center",
+        headerAlign: "center",
+        headerName: "Сумма",
+        field: "sum",
+        width: 200,
+        type: "number",
+      },
+      {
+        align: "center",
+        headerAlign: "center",
+        headerName: "Дата",
+        type: "date",
+        valueGetter: (params) =>
+          params.row.dt ? params.row.dt.toDate() : null,
+        field: "dt",
+        width: 200,
+      },
+      {
+        align: "center",
+        headerAlign: "center",
+        headerName: "Комментарий",
+        type: "string",
+        field: "dsc",
+        width: 400,
+      },
+      {
+        align: "center",
+        headerAlign: "center",
+        headerName: "Назначение платежа",
+        type: "singleSelect",
+        field: "purpose",
+        width: 400,
+        valueOptions: selectPurposes,
+      },
+    ],
+    [selectPurposes]
+  );
   return columns;
 }
