b531c50494f1539baaa45ce2729a4444364b9beb
Ibah <starlord77@mail.ru>
2023-07-13 15:30:05 +0300

    refactor(createTheme): поменял размер шрифта

--- src/lib/createTheme.ts
+++ src/lib/createTheme.ts
@@ -1,20 +1,27 @@
-import { createTheme as createThemeOrigin, PaletteMode } from "@mui/material";
+import {
+  createTheme as createThemeOrigin,
+  PaletteMode,
+  responsiveFontSizes,
+} from "@mui/material";
 import { ruRU } from "@mui/material/locale";
 import { ruRU as ruRUGrid } from "@mui/x-data-grid-premium";
 export default function createTheme(mode?: PaletteMode) {
-  return createThemeOrigin(
-    {
-      palette: {
-        mode,
-      },
-      components: {
-        MuiButton: { defaultProps: { color: "primary" } },
-        MuiButtonGroup: {
-          defaultProps: { color: "primary" },
+  return responsiveFontSizes(
+    createThemeOrigin(
+      {
+        palette: {
+          mode,
+        },
+        typography: { fontSize: 12 },
+        components: {
+          MuiButton: { defaultProps: { color: "primary" } },
+          MuiButtonGroup: {
+            defaultProps: { color: "primary" },
+          },
         },
       },
-    },
-    ruRU,
-    ruRUGrid
+      ruRU,
+      ruRUGrid
+    )
   );
 }
