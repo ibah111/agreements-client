5f906d7fbaec2021d6e0634b0c5cbd039019b837
Ibah <starlord77@mail.ru>
2023-07-13 15:41:22 +0300

    refactor(appbar): сделал заготовку для  функционала переключения между таблицами

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,10 +1,25 @@
-import { Grid } from "@mui/material";
+import { AppBar, Grid, Toolbar, Typography } from "@mui/material";
 import AgreementTable from "./Table/Table";
 
 export default function Main() {
   return (
     <>
       <Grid container height={"100vh"} direction={"column"}>
+        <Grid sx={{}} item>
+          <AppBar position="static" sx={{ height: 50 }}>
+            <Toolbar>
+              <Grid>
+                <Typography
+                  variant="h5"
+                  noWrap={false}
+                  sx={{ display: { xs: "none", sm: "block" } }}
+                >
+                  {"ПО Соглашения"}
+                </Typography>
+              </Grid>
+            </Toolbar>
+          </AppBar>
+        </Grid>
         <AgreementTable />
       </Grid>
     </>
