0bcb5a147bc0a94691d9cbdbdfcc5b61529be8b2
ibah <starlord77@mail.ru>
2023-07-28 17:48:25 +0300

    chore: переместил api в другую папку
    +изменил импорты

--- src/api/PersonPropertiesLink/getPersonProperty.ts
+++ src/api/PersonPropertiesLink/getPersonProperty.ts
@@ -1,8 +1,8 @@
 import { authRetry, get, transformAxios } from "@tools/rxjs-pipes";
 import { of } from "rxjs";
-import { baseRequest } from "../utils/baseRequest";
+import { baseRequest } from "../../utils/baseRequest";
 import { PersonProperty } from "@contact/models";
-import { transformError } from "../utils/processError";
+import { transformError } from "../../utils/processError";
 
 export default function getPersonProperty(person_id: number) {
   return of(`PersonProperty/${person_id}`).pipe(
