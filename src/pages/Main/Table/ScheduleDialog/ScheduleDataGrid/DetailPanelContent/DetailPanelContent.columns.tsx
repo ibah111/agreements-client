6da5ee007d587c68fc4d0eebf98aa9b13a006aa2
ibah <starlord77@mail.ru>
2023-08-18 10:05:54 +0300

    refactor: дорабатываю таблицу

--- src/pages/Main/Table/ScheduleDialog/ScheduleDataGrid/DetailPanelContent/DetailPanelContent.columns.tsx
+++ src/pages/Main/Table/ScheduleDialog/ScheduleDataGrid/DetailPanelContent/DetailPanelContent.columns.tsx
@@ -1,37 +1,66 @@
 import { GridColDef } from "@mui/x-data-grid-premium";
 import { DebtCalcInstance } from "../../../../../../Models/DebtCalc";
+import useDict from "../../../../../../Hooks/useDict";
+import React from "react";
 
-export const detailPanelColumns: GridColDef<DebtCalcInstance>[] = [
-  {
-    field: "calc_date",
-    align: "center",
-    headerAlign: "center",
-    headerName: "Дата платежа",
-    width: 150,
-  },
-  {
-    field: "sum",
-    align: "center",
-    headerAlign: "center",
-    headerName: "Сумма",
-    width: 150,
-  },
-  {
-    field: "purpose",
-    align: "center",
-    headerAlign: "center",
-    headerName: "Назначение",
-    width: 150,
-    valueGetter(params) {
-      const t = params.row.purpose;
-      return t;
-    },
-  },
-  {
-    field: "dt",
-    align: "center",
-    headerAlign: "center",
-    headerName: "Дата занесения в кск",
-    width: 150,
-  },
-];
+export default function useDetailCols() {
+  const purposes = useDict(130);
+  const selectPurposes = React.useMemo(
+    () =>
+      purposes.map((item) => ({
+        value: item.code,
+        label: item.name,
+      })),
+    [purposes]
+  );
+  const detailPanelColumns = React.useMemo<GridColDef<DebtCalcInstance>[]>(
+    () => [
+      {
+        field: "id",
+        headerName: "ID",
+        headerAlign: "center",
+        align: "center",
+      },
+      {
+        field: "calc_date",
+        align: "center",
+        headerAlign: "center",
+        headerName: "Дата платежа",
+        width: 150,
+        type: "date",
+        valueGetter(params) {
+          return params.row.calc_date ? params.row.calc_date.toDate() : null;
+        },
+      },
+      {
+        field: "sum",
+        align: "center",
+        headerAlign: "center",
+        headerName: "Сумма",
+        width: 150,
+      },
+      {
+        field: "purpose",
+        align: "center",
+        headerAlign: "center",
+        headerName: "Назначение",
+        width: 150,
+        type: "singleSelect",
+        valueOptions: selectPurposes,
+      },
+      {
+        field: "dt",
+        align: "center",
+        headerAlign: "center",
+        headerName: "Дата занесения в КСК",
+        width: 150,
+        type: "date",
+        valueGetter(params) {
+          return params.row.dt ? params.row.dt.toDate() : null;
+        },
+      },
+    ],
+    [selectPurposes]
+  );
+  return { detailPanelColumns };
+}
