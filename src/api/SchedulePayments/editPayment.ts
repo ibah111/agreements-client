00bebc5bfe4b6f7c9913c458825daed762a78dca
ibah <starlord77@mail.ru>
2023-08-30 11:08:51 +0300

    feat: edit payment api

---
+++ src/api/SchedulePayments/editPayment.ts
@@ -0,0 +1,18 @@
+import { of } from "rxjs";
+import { baseRequest } from "../../utils/baseRequest";
+import { authRetry, patch, transformAxios } from "@tools/rxjs-pipes";
+import { transformError } from "../../utils/processError";
+
+interface EditInput {
+  pay_day: Date;
+  sum_owe: number;
+}
+
+export default function editPayment(id_payment: number, data: EditInput) {
+  return of(data).pipe(
+    patch(baseRequest, `/Payments/updateCalc/${id_payment}`),
+    transformAxios(),
+    transformError(),
+    authRetry()
+  );
+}
