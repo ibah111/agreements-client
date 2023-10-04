1c67a552ff264829cdf11984b1759c5d4cbf1942
Ibah <starlord77@mail.ru>
2023-10-04 16:50:57 +0300

    feat(download-filter-button): доделал кнопку скачки фильтра

--- src/pages/Main/Table/ToolBar/DownloadFilterButton.tsx
+++ src/pages/Main/Table/ToolBar/DownloadFilterButton.tsx
@@ -3,10 +3,23 @@ import Button from "@mui/material/Button";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import StyleIcon from "@mui/icons-material/Style";
-import { useGridApiContext, useGridApiRef } from "@mui/x-data-grid-premium";
+import {
+  useGridApiContext,
+  gridFilterModelSelector,
+  useGridSelector,
+  gridSortModelSelector,
+  GridSortModel,
+  GridFilterModel,
+} from "@mui/x-data-grid-premium";
 import DownloadIcon from "@mui/icons-material/Download";
 import FileUploadIcon from "@mui/icons-material/FileUpload";
 import { Typography } from "@mui/material";
+function downloadFile(name: string, uri: string) {
+  const a = document.createElement("a");
+  a.download = name;
+  a.href = uri;
+  a.click();
+}
 export default function DownloadFilterButton() {
   /**
    * Базовый код, чтобы работало меню в тулбаре
@@ -20,23 +33,36 @@ export default function DownloadFilterButton() {
     setAnchorEl(null);
   };
   /**
-   * Инициилизирую блов
-   */
-  const blob = new Blob();
-
-  const onDownload = () => {
-    const link = document.createElement("a");
-    link.download = `download.txt`;
-    link.href = "./download.txt";
-    link.click();
-  };
-  /**
    * апи референс к текущему контексту
    */
-  const apiRef = useGridApiRef();
-  // const currentRef = apiRef.current.setFilterModel();
   const apiContext = useGridApiContext();
-  // const currentContext = apiContext.current.setFilterModel();
+  const filterModel = useGridSelector(apiContext, gridFilterModelSelector);
+  const sortModel = useGridSelector(apiContext, gridSortModelSelector);
+  const click = React.useCallback(() => {
+    downloadFile(
+      "фильтр.import",
+      URL.createObjectURL(
+        new Blob([btoa(JSON.stringify({ filterModel, sortModel }))])
+      )
+    );
+  }, [filterModel, sortModel]);
+  //TODO добавить проверку
+  const upload = React.useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const file = event.target?.files?.[0];
+      if (file) {
+        file.text().then((text) => {
+          const data: {
+            filterModel: GridFilterModel;
+            sortModel: GridSortModel;
+          } = JSON.parse(atob(text));
+          apiContext.current.setFilterModel(data.filterModel);
+          apiContext.current.setSortModel(data.sortModel);
+        });
+      }
+    },
+    [apiContext]
+  );
   return (
     <div>
       <Button
@@ -59,19 +85,14 @@ export default function DownloadFilterButton() {
           "aria-labelledby": "basic-button",
         }}
       >
-        <MenuItem onClick={onDownload}>
+        <MenuItem onClick={click}>
           <DownloadIcon />
           <Typography>Скачать</Typography>
         </MenuItem>
-        <MenuItem
-          onClick={() => {
-            /**
-             * openDialog
-             */
-          }}
-        >
+        <MenuItem component="label">
           <FileUploadIcon />
           <Typography>Загрузить</Typography>
+          <input type="file" hidden onChange={upload} accept="*.import" />
         </MenuItem>
       </Menu>
       {}
