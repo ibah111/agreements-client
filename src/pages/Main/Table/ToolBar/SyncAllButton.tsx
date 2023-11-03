fa55145eb1b6ba4e9e70a3bca23db40e403848ed
Ibah <starlord77@mail.ru>
2023-11-03 15:44:50 +0300

    feat(syncAll):  доработал кнопку синхронизации всего

--- src/pages/Main/Table/ToolBar/SyncAllButton.tsx
+++ src/pages/Main/Table/ToolBar/SyncAllButton.tsx
@@ -1,27 +1,48 @@
-import { Button } from "@mui/material";
 import { Can } from "../../../../casl/casl";
 import { Action, Subject } from "../../../../casl/casl.factory";
 import UpdateIcon from "@mui/icons-material/Update";
 import { enqueueSnackbar } from "notistack";
 import syncAll from "../../../../api/Preview/syncAll";
+import React from "react";
+import { Subject as RxSubject, takeUntil } from "rxjs";
+import { LoadingButton } from "@mui/lab";
 interface UpdateProps {
   refresh: VoidFunction;
 }
-export default function SyncAllButton(props: UpdateProps) {
+export default function SyncAllButton({ refresh }: UpdateProps) {
+  const [loading, setLoading] = React.useState(false);
+  const subject = React.useMemo(() => new RxSubject<null>(), []);
+  const cancel = React.useCallback(() => {
+    subject.next(null);
+  }, [subject]);
+  const update = React.useCallback(() => {
+    setLoading(true);
+    const sub = syncAll()
+      .pipe(takeUntil(subject))
+      .subscribe({
+        next: () => {
+          enqueueSnackbar(`Соглашения обновлены`, { variant: "info" });
+          refresh();
+        },
+        complete: () => {
+          setLoading(false);
+        },
+      });
+    return () => {
+      sub.unsubscribe();
+    };
+  }, [refresh, subject]);
   return (
     <Can I={Action.Create} a={Subject.Agreement}>
-      <Button
+      <LoadingButton
         startIcon={<UpdateIcon />}
         size="small"
         variant="contained"
-        color={"primary"}
-        onClick={() => {
-          syncAll().subscribe(() => {
-            enqueueSnackbar(`Соглашения обновлены`, { variant: "info" });
-            props.refresh();
-          });
-        }}
-      >{`Обновить данные`}</Button>
+        color={loading ? "error" : "primary"}
+        onClick={loading ? cancel : update}
+      >
+        {loading ? "Отмена" : `Обновить данные`}
+      </LoadingButton>
     </Can>
   );
 }
